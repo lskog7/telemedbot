3549e6ab6bc02748732000087b4fe56d4c7d8b50
Aleksei Sidorov <53901048+walkingfires@users.noreply.github.com>
2024-03-24 16:35:33 +0300

    new update phase registration

--- bot_requests.py
+++ bot_requests.py
@@ -4,12 +4,15 @@ from peewee import JOIN
 class Requests:
 
     @staticmethod
+    def user_in_db(user):
+        return len(User.select(User.telegram_id).where(User.telegram_id == user))
+
+
+    @staticmethod
     def write_user(telegram_id):
-        users = User.select().where(User.telegram_id == telegram_id)
-        if not users.count():
-            u = User(telegram_id=telegram_id, age=0, sex=2, result=0)
-            u.save()
-            return u
+        u = User(telegram_id=telegram_id, sex=2)
+        u.save()
+        return u
 
     @staticmethod
     def user_age(telegram_id):
@@ -33,107 +36,107 @@ class Requests:
         u.sex = sex
         u.save()
 
-    @staticmethod
-    def get_user_next_question(telegram_id):  # Очередной неотвеченный вопрос
-        q1 = (Question
-              .select()
-              .join(Answer, join_type=JOIN.LEFT_OUTER, on=(Answer.question_id == Question.id))
-              .join(UserAnswer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == UserAnswer.answer_id))
-              .where(UserAnswer.user == User.get(User.telegram_id == telegram_id))
-              )
-
-        q = Question.select().except_(q1)
-        if q.count():
-            return q[0]
-
-    @staticmethod
-    def get_question_answers(question):  # Все варианты ответов на вопрос
-        return Answer.select().where(Answer.question == question)
-
-    # Очередной неотвеченный вопрос и его варианты ответа
-    @staticmethod
-    def get_next_user_question_and_answers(telegram_id):
-        question = Requests.get_user_next_question(telegram_id)
-        if question:
-            text = Question.select().where(Question.id == question)[0].text
-            return text, Requests.get_question_answers(question)
-        else:
-            return 0
-
-    @staticmethod
-    def user_has_answer_on_same_question(telegram_id, answer_id):
-        user = User.get(User.telegram_id == telegram_id)
-        answer = Answer.get(Answer.id == answer_id)
-        question = answer.question
-        questions = (Question
-                     .select()
-                     .join(Answer, join_type=JOIN.LEFT_OUTER, on=(Question.id == Answer.question_id))
-                     .join(UserAnswer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == UserAnswer.answer_id))
-                     .where(UserAnswer.user_id == user))
-        return question in questions
-
-    # Подсчет баллов для каждого элемента, исходя из ответов пользователя, и запись результата
-    @staticmethod
-    def calc_user_grade(telegram_id):
-        user = User.get(User.telegram_id == telegram_id)
-        user_answer_elements = (AnswerElement
-                                .select().
-                                join(Answer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == AnswerElement.answer_id))
-                                .join(UserAnswer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == UserAnswer.answer_id))
-                                .where(UserAnswer.user == user))
-        result = ''
-        for element in Element.select():
-            percent = sum([answer_element.grade for answer_element in user_answer_elements.select().where(
-                AnswerElement.element == element)]) / element.max_grade * 100
-            if percent >= element.edge_grade:
-                result += str(element)
-        if result == '':
-            result, user.result = 100, 100
-        else:
-            user.result = int(result)
-        user.save()
-        return result
-
-    @staticmethod
-    def custom_answer(result):
-        if result == 100:
-            q = """Ваши ответы были успешно обработаны
-                   В результате тестирования не было выявлено наличие дефицита
-                   ‼️ Результат тестирования не является медицинским диагнозом"""
-        else:
-            elements = ''
-            for element in Element.select():
-                if element.id in list(map(int, str(result))):
-                    elements += element.name + ', '
-            q = f"""Ваши ответы были успешно обработаны
-                    В результате тестирования было выявлено наличие дефицита {elements[:-2]}
-                    Предлагаем вам ознакомиться с рекомендациями, представленным ниже.
-                    ‼️ Результат тестирования не является медицинским диагнозом"""
-        return q
-
-    @staticmethod
-    def custom_tips(chat_id, result):
-        if not result == 100:
-            photos = []
-            for element in Element.select():
-                if element.id in list(map(int, str(result))):
-                    photos.append(types.InputMediaPhoto(open(f'tips/{element.name}.png', 'rb')))
-            bot.send_media_group(chat_id, photos)
-
-    @staticmethod
-    def last_result(telegram_id):
-        user = User.get(User.telegram_id == telegram_id)
-        result = user.result
-        if result == 0:
-            q = 'Ни одного тестирования не было пройдено'
-        elif result == 100:
-            q = """В результате тестирования не было выявлено наличие дефицита
-‼️ Результат тестирования не является медицинским диагнозом"""
-        else:
-            elements = ''
-            for element in Element.select():
-                if element.id in list(map(int, str(result))):
-                    elements += element.name + ', '
-            q = f"""В результате тестирования было выявлено наличие дефицита {elements[:-2]}
-‼️ Результат тестирования не является медицинским диагнозом"""
-        return q
+#     @staticmethod
+#     def get_user_next_question(telegram_id):  # Очередной неотвеченный вопрос
+#         q1 = (Question
+#               .select()
+#               .join(Answer, join_type=JOIN.LEFT_OUTER, on=(Answer.question_id == Question.id))
+#               .join(UserAnswer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == UserAnswer.answer_id))
+#               .where(UserAnswer.user == User.get(User.telegram_id == telegram_id))
+#               )
+#
+#         q = Question.select().except_(q1)
+#         if q.count():
+#             return q[0]
+#
+#     @staticmethod
+#     def get_question_answers(question):  # Все варианты ответов на вопрос
+#         return Answer.select().where(Answer.question == question)
+#
+#     # Очередной неотвеченный вопрос и его варианты ответа
+#     @staticmethod
+#     def get_next_user_question_and_answers(telegram_id):
+#         question = Requests.get_user_next_question(telegram_id)
+#         if question:
+#             text = Question.select().where(Question.id == question)[0].text
+#             return text, Requests.get_question_answers(question)
+#         else:
+#             return 0
+#
+#     @staticmethod
+#     def user_has_answer_on_same_question(telegram_id, answer_id):
+#         user = User.get(User.telegram_id == telegram_id)
+#         answer = Answer.get(Answer.id == answer_id)
+#         question = answer.question
+#         questions = (Question
+#                      .select()
+#                      .join(Answer, join_type=JOIN.LEFT_OUTER, on=(Question.id == Answer.question_id))
+#                      .join(UserAnswer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == UserAnswer.answer_id))
+#                      .where(UserAnswer.user_id == user))
+#         return question in questions
+#
+#     # Подсчет баллов для каждого элемента, исходя из ответов пользователя, и запись результата
+#     @staticmethod
+#     def calc_user_grade(telegram_id):
+#         user = User.get(User.telegram_id == telegram_id)
+#         user_answer_elements = (AnswerElement
+#                                 .select().
+#                                 join(Answer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == AnswerElement.answer_id))
+#                                 .join(UserAnswer, join_type=JOIN.LEFT_OUTER, on=(Answer.id == UserAnswer.answer_id))
+#                                 .where(UserAnswer.user == user))
+#         result = ''
+#         for element in Element.select():
+#             percent = sum([answer_element.grade for answer_element in user_answer_elements.select().where(
+#                 AnswerElement.element == element)]) / element.max_grade * 100
+#             if percent >= element.edge_grade:
+#                 result += str(element)
+#         if result == '':
+#             result, user.result = 100, 100
+#         else:
+#             user.result = int(result)
+#         user.save()
+#         return result
+#
+#     @staticmethod
+#     def custom_answer(result):
+#         if result == 100:
+#             q = """Ваши ответы были успешно обработаны
+#                    В результате тестирования не было выявлено наличие дефицита
+#                    ‼️ Результат тестирования не является медицинским диагнозом"""
+#         else:
+#             elements = ''
+#             for element in Element.select():
+#                 if element.id in list(map(int, str(result))):
+#                     elements += element.name + ', '
+#             q = f"""Ваши ответы были успешно обработаны
+#                     В результате тестирования было выявлено наличие дефицита {elements[:-2]}
+#                     Предлагаем вам ознакомиться с рекомендациями, представленным ниже.
+#                     ‼️ Результат тестирования не является медицинским диагнозом"""
+#         return q
+#
+#     @staticmethod
+#     def custom_tips(chat_id, result):
+#         if not result == 100:
+#             photos = []
+#             for element in Element.select():
+#                 if element.id in list(map(int, str(result))):
+#                     photos.append(types.InputMediaPhoto(open(f'tips/{element.name}.png', 'rb')))
+#             bot.send_media_group(chat_id, photos)
+#
+#     @staticmethod
+#     def last_result(telegram_id):
+#         user = User.get(User.telegram_id == telegram_id)
+#         result = user.result
+#         if result == 0:
+#             q = 'Ни одного тестирования не было пройдено'
+#         elif result == 100:
+#             q = """В результате тестирования не было выявлено наличие дефицита
+# ‼️ Результат тестирования не является медицинским диагнозом"""
+#         else:
+#             elements = ''
+#             for element in Element.select():
+#                 if element.id in list(map(int, str(result))):
+#                     elements += element.name + ', '
+#             q = f"""В результате тестирования было выявлено наличие дефицита {elements[:-2]}
+# ‼️ Результат тестирования не является медицинским диагнозом"""
+#         return q
--- telehack_bot.py
+++ telehack_bot.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot.types import Message
+from bot_requests import *
 from telebot import types
 
 from db_model import User, Test, Specialist, Result, Question, Answer, UserAnswer
@@ -7,23 +8,99 @@ from peewee import JOIN
 
 bot = telebot.TeleBot('7077776032:AAGJLvl7VZVzmGXDPX6nqIEbetTLSQcuJm8', num_threads=10)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    user = call.from_user.id
+    chat_id = call.message.chat.id
+    message_id = call.message.message_id
+    if call.data == 'roll_up':
+        bot.edit_message_reply_markup(chat_id, message_id)
+    elif call.data == 'come_back':
+        Call.edit_menu(chat_id, message_id)
+    elif call.data == 'yes-name':
+        bot.send_message(user, text=emoji() + 'Выберите пол', reply_markup=small_keyboard('sex'))
+    elif call.data == 'no-name':
+        bot.send_message(user, text=emoji() + 'Выберите пол', reply_markup=small_keyboard('sex'))
+    # elif call.data == 'new_test':
+    #     bot.edit_message_reply_markup(chat_id, message_id, reply_markup=Call.new_test())
+    # elif call.data == 'start_test':
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     Call.question(Requests.get_next_user_question_and_answers(user), chat_id, message_id)
+    # elif call.data == 'result':
+    #     bot.edit_message_text(Requests.last_result(user), chat_id, message_id)
+    #     bot.edit_message_reply_markup(chat_id, message_id, reply_markup=Call.result())
+    # elif call.data == 'parameters':
+    #     Call.edit_parameters(user, chat_id, message_id)
+    # elif call.data == 'feedback':
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     bot.send_message(user, emoji() + 'Напишите Ваш отзыв')
+    #     bot.register_next_step_handler(call.message, Get.feedback)
+    # elif call.data == 'mineralka':
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     Call.mineralka(user)
+    # elif call.data == 'mineralka_roll_up':
+    #     bot.delete_message(chat_id, message_id)
+    # elif call.data == 'delete_roll_up':
+    #     bot.delete_message(chat_id, message_id)
+    #     Call.menu(user)
+    # elif call.data == 'edit_parameters':
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     bot.send_message(user, text=emoji() + 'Выберите пол', reply_markup=small_keyboard('parameters'))
+    # elif call.data == "sex_male":
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     bot.send_message(user, emoji() + 'Введите возраст')
+    #     Requests.save_user_sex(user, 0)
+    #     bot.register_next_step_handler(call.message, Get.start_age)
+    # elif call.data == "sex_female":
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     bot.send_message(user, emoji() + 'Введите возраст')
+    #     Requests.save_user_sex(user, 1)
+    #     bot.register_next_step_handler(call.message, Get.start_age)
+    # elif call.data == 'parameters_male':
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     bot.send_message(user, emoji() + 'Введите возраст')
+    #     Requests.save_user_sex(user, 0)
+    #     bot.register_next_step_handler(call.message, Get.parameters_age)
+    # elif call.data == 'parameters_female':
+    #     bot.edit_message_reply_markup(chat_id, message_id)
+    #     bot.send_message(user, emoji() + 'Введите возраст')
+    #     Requests.save_user_sex(user, 1)
+    #     bot.register_next_step_handler(call.message, Get.parameters_age)
 
 @bot.message_handler(commands=['start', 'menu'])
 def registration(message: Message):
     print(message)
     user = message.from_user.id
-    if user not in user_list:
+    if not Requests.user_in_db(user):
         if message.from_user.is_bot is False:
-            q = emoji() + 'Привет, Я Минералка - бот, который следит за твоим здоровьем. Тебе не хочется обращаться в больницу, стоять в длинных очередях, сдавать множество анализов и проходить большое количество обследований? Тогда я помогу тебе!'
+            q = emoji() + 'Привет'
             bot.send_message(user, text=q)
-            bot.send_message(user, text=emoji() + 'Выберите пол', reply_markup=small_keyboard('sex'))
+            first_name = message.from_user.first_name
+            last_name = message.from_user.last_name
             Requests.write_user(user)
-            user_list.append(user)
+            if last_name is not None:
+                bot.send_message(user, text=emoji() + f'Вас зовут {last_name} {first_name}?', reply_markup=small_keyboard('name'))
+            else:
+                bot.send_message(user, text=emoji() + f'Вас зовут {first_name}?', reply_markup=small_keyboard('name'))
+
     else:
-        Call.menu(user)
+         Call.menu(user)
 
 def emoji():
     return '💊‍ '
+
+def small_keyboard(keyboard_type):
+    keyboard = types.InlineKeyboardMarkup()
+    if keyboard_type == 'name':
+        keyboard.add(types.InlineKeyboardButton(text='Да', callback_data='yes_name'),
+                     types.InlineKeyboardButton(text='Нет', callback_data='no_name'))
+    # if keyboard_type == 'sex':  # да или нет для первоначального выбора имени
+    #     keyboard.add(types.InlineKeyboardButton(text='Мужской', callback_data='sex_male'),
+    #                  types.InlineKeyboardButton(text='Женский', callback_data='sex_female'))
+    # if keyboard_type == 'parameters':  # да или нет для первоначального выбора имени
+    #     keyboard.add(types.InlineKeyboardButton(text='Мужской', callback_data='parameters_male'),
+    #                  types.InlineKeyboardButton(text='Женский', callback_data='parameters_female'))
+    return keyboard
 
 if __name__ == '__main__':
     print(bot.get_me())
