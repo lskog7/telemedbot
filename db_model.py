954de8a69d1c0069cce0096648316942090d4df7
lskog7 <tematema011@gmail.com>
2024-03-30 22:11:43 +0300

    refactored db_model

--- db_model.py
+++ db_model.py
@@ -1,54 +1,62 @@
-from peewee import CharField, ForeignKeyField, IntegerField, Model, TextField, DateTimeField, SqliteDatabase
+from peewee import (
+    CharField,
+    ForeignKeyField,
+    IntegerField,
+    Model,
+    TextField,
+    DateTimeField,
+    SqliteDatabase,
+)
 
-# У всех моделей автоматически создается также поле id: PRIMARY KEY AUTOINCREMENT
+# У всех моделей автоматически создается также поле id: PRIMARY KEY AUTO_INCREMENT
 
-db = SqliteDatabase('telehack.db')  # Наша бд
+db = SqliteDatabase("telehack.db")  # Наша бд
+
 
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 # -------------------------------Таблицы--------------------------------
 
-class User(BaseModel):  # Пользователь
-    SEX_CHOICES = (
-        (0, 'Мужской'),
-        (1, 'Женский'),
-        (2, 'Не указан')
-    )
+
+class Users(BaseModel):  # Пользователь
+    SEX_CHOICES = ((0, "Не указан"), (1, "Мужской"), (2, "Женский"))
 
     telegram_id = IntegerField(unique=True)
-    sex = IntegerField(null=True, choices=SEX_CHOICES, default=2)
-    name = CharField(max_length=30)
-    surname = CharField(max_length=30)
-    patronymic = CharField(max_length=30)
+    sex = IntegerField(null=True, choices=SEX_CHOICES, default=0)
+    name = CharField(max_length=256)
+    surname = CharField(max_length=256)
+    patronymic = CharField(max_length=256)
     b_date = DateTimeField()
 
 
-class Test(BaseModel):  # Данные об исследовании
-    user_id = ForeignKeyField(User, null=True)
+class Tests(BaseModel):  # Данные об исследовании
+    user_id = ForeignKeyField(Users, null=True)
     date = DateTimeField(null=True)
-    text_info = TextField()
+    add_info = TextField()
+    curr_quest = IntegerField()
 
 
-class Specialist(BaseModel): # Данные о специалисте
-    field = CharField(max_length=50, null=True)
+class Specialists(BaseModel):  # Данные о специалисте
+    info = CharField(max_length=50, null=True)
 
 
-class Result(BaseModel): # Результат исследования
-    test_id = ForeignKeyField(Test, null=True)
-    specialist_id = ForeignKeyField(Specialist, null=True)
+class Results(BaseModel):  # Результат исследования
+    test_id = ForeignKeyField(Tests, null=True)
+    specialist_id = ForeignKeyField(Specialists, null=True)
 
 
-class Question(BaseModel):  # Вопрос
+class Questions(BaseModel):  # Вопросы
     text = TextField()
 
 
-class Answer(BaseModel):  # Вариант ответа
-    text = TextField()
-    question = ForeignKeyField(Question)
+class Answers(BaseModel):  # Вариант ответа
+    quest_id = ForeignKeyField(Questions)
+    answer = CharField(max_length=50)
 
 
-class UserAnswer(BaseModel):  # Связка варианта ответа и исследования
-    test_id = ForeignKeyField(Test, null=True)
-    answer_id = ForeignKeyField(Answer, null=True)
+class UserAnswers(BaseModel):  # Ответы пользователя
+    test_id = ForeignKeyField(Tests, null=True)
+    answer_id = ForeignKeyField(Answers, null=True)
