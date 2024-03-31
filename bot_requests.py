36c690bc3a36bff6693475c17d4e2c0451e5c38e
Aleksei Sidorov <53901048+walkingfires@users.noreply.github.com>
2024-04-01 00:13:28 +0300

    new crypto funcs

--- bot_requests.py
+++ bot_requests.py
@@ -8,66 +8,66 @@ class Requests:
 
     # Проверка наличия пользователя в БД по id
     @staticmethod
-    def users_in_db(telegram_id, key):
-        count = Users.select(Users.telegram_id).where(Users.telegram_id == encrypt(telegram_id, key)).count()
+    def users_in_db(telegram_id):
+        count = Users.select(Users.telegram_id).where(Users.telegram_id == telegram_id).count()
         return count
 
     # Запись новой строки в таблицу users
     @staticmethod
-    def write_user(telegram_id, key):
+    def write_user(telegram_id):
         # Записываем новую строчку в БД
-        query = Users(telegram_id=encrypt(telegram_id, key), sex=0)
+        query = Users(telegram_id=telegram_id, sex=0)
         query.save()
 
     # Возвращает пол выбранного пользователя
     @staticmethod
-    def get_user_sex(telegram_id, key):
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
+    def get_user_sex(telegram_id):
+        query = Users.select().where(Users.telegram_id == telegram_id)
         return Users.SEX_CHOICES[query[0].sex][1]
 
     # Возвращает имя выбранного пользователя
     @staticmethod
-    def get_user_name(telegram_id, key):
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
-        return decrypt(query[0].name, key)
+    def get_user_name(telegram_id, key, iv):
+        query = Users.select().where(Users.telegram_id == telegram_id)
+        return decrypt(query[0].name, key, iv)
 
     # Возвращает фамилию выбранного пользователя
     @staticmethod
-    def get_user_surname(telegram_id, key):
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
-        return decrypt(query[0].surname, key)
+    def get_user_surname(telegram_id, key, iv):
+        query = Users.select().where(Users.telegram_id == telegram_id)
+        return decrypt(query[0].surname, key, iv)
 
     # Возвращает отчество выбранного пользователя
     @staticmethod
-    def get_user_patronymic(telegram_id, key):
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
-        return decrypt(query[0].patronymic, key)
+    def get_user_patronymic(telegram_id, key, iv):
+        query = Users.select().where(Users.telegram_id == telegram_id)
+        return decrypt(query[0].patronymic, key, iv)
 
     # Возвращает дату рождения выбранного пользователя
     @staticmethod
-    def get_user_b_date(telegram_id, key):
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
+    def get_user_b_date(telegram_id):
+        query = Users.select().where(Users.telegram_id == telegram_id)
         return query[0].b_date
 
     # Сохраняет имя пользователя
     @staticmethod
-    def save_user_name(telegram_id, name, key):
-        query = Users.get(Users.telegram_id == encrypt(telegram_id, key))
-        query.name = encrypt(name, key)
+    def save_user_name(telegram_id, name, key, iv):
+        query = Users.get(Users.telegram_id == telegram_id)
+        query.name = encrypt(name, key, iv)
         query.save()
 
     # Сохраняет фамилия пользователя
     @staticmethod
-    def save_user_surname(telegram_id, surname, key):
-        query = Users.get(Users.telegram_id == encrypt(telegram_id, key))
-        query.surname = encrypt(surname, key)
+    def save_user_surname(telegram_id, surname, key, iv):
+        query = Users.get(Users.telegram_id == telegram_id)
+        query.surname = encrypt(surname, key, iv)
         query.save()
 
     # Сохраняет отчество пользователя
     @staticmethod
-    def save_user_patronymic(telegram_id, patronymic, key):
-        query = Users.get(Users.telegram_id == encrypt(telegram_id, key))
-        query.patronymic = encrypt(patronymic, key)
+    def save_user_patronymic(telegram_id, patronymic, key, iv):
+        query = Users.get(Users.telegram_id == telegram_id)
+        query.patronymic = encrypt(patronymic, key, iv)
         query.save()
 
     # Сохраняет пол пользователя
@@ -86,8 +86,8 @@ class Requests:
 
     # Возвращает номер последнего теста и вопроса для пользователя (если нет, то 0, 0)
     @staticmethod
-    def get_user_next_question(telegram_id, key):  # Очередной неотвеченный вопрос
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
+    def get_user_next_question(telegram_id):  # Очередной неотвеченный вопрос
+        query = Users.select().where(Users.telegram_id == telegram_id)
         current_test = query[0].current_test
         current_question = query[0].current_question
         if current_test != 0:
@@ -122,7 +122,7 @@ class Requests:
 
     # Очередной неотвеченный вопрос (номер) и его варианты ответа (лист)
     @staticmethod
-    def get_next_user_question_and_answers(telegram_id, key):
+    def get_next_user_question_and_answers(telegram_id):
         current_test, current_question = Requests.get_user_next_question(telegram_id)
         if current_test != 0 and current_question != 0:
             question_text = Requests.get_question_text(current_question)
@@ -137,8 +137,8 @@ class Requests:
 
     # Возвращает список тестов пользователя
     @staticmethod
-    def get_user_tests(telegram_id, key):
-        query1 = Users.select(Users.id).where(Users.telegram_id == encrypt(telegram_id, key))
+    def get_user_tests(telegram_id):
+        query1 = Users.select(Users.id).where(Users.telegram_id == telegram_id)
         if len(query1) == 0:
             return -1
         user_id = query1[0].id
@@ -152,8 +152,8 @@ class Requests:
 
     # Возвращает список результатов пользователя (id специалистов)
     @staticmethod
-    def get_user_result(telegram_id, test_num, key):
-        user_tests = Requests.get_user_tests(encrypt(telegram_id, key))
+    def get_user_result(telegram_id, test_num):
+        user_tests = Requests.get_user_tests(telegram_id)
         if user_tests != -1:
             if len(user_tests) < test_num:
                 query = Results.select(Results.specialist_id).where(
@@ -180,20 +180,20 @@ class Requests:
 
     # Возвращает описание специалистов для выбранного юзера и выбранного теста
     @staticmethod
-    def get_user_specialists(telegram_id, test_num, key):
-        specialists_ids = Requests.get_user_result(encrypt(telegram_id, key), test_num)
+    def get_user_specialists(telegram_id, test_num):
+        specialists_ids = Requests.get_user_result(telegram_id, test_num)
         specialists_info = Requests.get_specialists_info(specialists_ids)
         return specialists_info
 
     @staticmethod
-    def get_user_info(telegram_id, key):
-        query = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
+    def get_user_info(telegram_id, key, iv):
+        query = Users.select().where(Users.telegram_id == telegram_id)
         if len(query) != 0:
             user = query[0]
             info = [0 for _ in range(5)]
-            info[0] = decrypt(user.name, key)
-            info[1] = decrypt(user.surname, key)
-            info[2] = decrypt(user.patronymic, key)
+            info[0] = decrypt(user.name, key, iv)
+            info[1] = decrypt(user.surname, key, iv)
+            info[2] = decrypt(user.patronymic, key, iv)
             info[3] = user.sex
             info[4] = user.b_date
             return info
@@ -201,8 +201,8 @@ class Requests:
             return -1
 
     @staticmethod
-    def start_new_test(telegram_id, text_info="", key):
-        query1 = Users.select().where(Users.telegram_id == encrypt(telegram_id, key))
+    def start_new_test(telegram_id, text_info=""):
+        query1 = Users.select().where(Users.telegram_id == telegram_id)
         user_id = query1[0].id
         query2 = Tests(user_id=user_id, date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), text=text_info, curr_test=0)
         query2.save()
